15326d4c10b3606df5d20184f128a668103eaf89
Deportator <fabrichnov87@gmail.com>
2022-04-26 22:44:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+
+        int amount = 25000;
+        int oneMile = 20;
+
+        int miles = amount / oneMile;
+
+        System.out.println("Колличество бонусных миль:" + miles);
+
+        System.out.println("Bye!");
+
+    }
+}
